6459dd316802db18d5d2229dc1231a06379b7986
Thenexus96 <nsanford96@gmail.com>
2026-06-12 14:17:07 -0700

    Refactor drone communication and logging; update mod description and version

---
+++ src/main/java/net/sanfonic/hivemind/network/DebugNetworkConstants.java
@@ -0,0 +1,16 @@
+package net.sanfonic.hivemind.network;
+
+import net.minecraft.util.Identifier;
+import net.sanfonic.hivemind.Hivemind;
+
+public final class DebugNetworkConstants {
+    public static final Identifier DEBUG_SPAWN_PACKET =
+            new Identifier(Hivemind.MOD_ID, "debug_spawn");
+    public static final Identifier DEBUG_KILL_NEARBY_PACKET =
+            new Identifier(Hivemind.MOD_ID, "debug_kill_nearby");
+    public static final Identifier DEBUG_TELEPORT_PACKET =
+            new Identifier(Hivemind.MOD_ID, "debug_teleport");
+
+    private DebugNetworkConstants() {
+    }
+}
